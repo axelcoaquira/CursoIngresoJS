ef800284de13be6ef79ddd3d1f894e08f3aa2a14
axelcoaquira <47337987+axelcoaquira@users.noreply.github.com>
2019-02-14 16:58:14 -0300

    primer commit
    Ejercicio 7 terminado

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,69 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var primernumero;
+	var segundonumero;
+	var sumar;
+
+	primernumero=numeroUno.value;
+	segundonumero=numeroDos.value;
+
+	primernumero=parseInt(primernumero);
+	segundonumero=parseInt(segundonumero);
+
+	sumar=primernumero+segundonumero;
+
+	alert("La suma es: "+sumar);
 }
 
 function restar()
 {
-	
+	var primernumero;
+	var segundonumero;
+	var restar;
+
+	primernumero=numeroUno.value;
+	segundonumero=numeroDos.value;
+
+	primernumero=parseInt(primernumero);
+	segundonumero=parseInt(segundonumero);
+
+	restar=primernumero-segundonumero;
+
+	alert("La resta es: "+restar);
 }
 
 function multiplicar()
 { 
-	
+	var primernumero;
+	var segundonumero;
+	var multiplicar;
+
+	primernumero=numeroUno.value;
+	segundonumero=numeroDos.value;
+
+	primernumero=parseInt(primernumero);
+	segundonumero=parseInt(segundonumero);
+
+	multiplicar=primernumero*segundonumero;
+
+	alert("La multiplicacion es: "+multiplicar);
 }
 
 function dividir()
 {
-	
+	var primernumero;
+	var segundonumero;
+	var dividir;
+
+	primernumero=numeroUno.value;
+	segundonumero=numeroDos.value;
+
+	primernumero=parseInt(primernumero);
+	segundonumero=parseInt(segundonumero);
+
+	dividir=primernumero/segundonumero;
+
+	alert("La division es: "+dividir);
 }
 

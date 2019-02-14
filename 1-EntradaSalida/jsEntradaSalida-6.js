52581e8e7d70ccb0c9f283212c9d2c83c0c90b75
axelcoaquira <47337987+axelcoaquira@users.noreply.github.com>
2019-02-14 16:43:17 -0300

    primer commit
    Ejercicio 6 terminado

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,18 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+  var primernumero;
+  var segundonumero;
+  var suma;
 
+  primernumero = numeroUno.value;
+  segundonumero = numeroDos.value;
+
+  primernumero = parseInt(primernumero);
+  segundonumero = parseInt(segundonumero);
+
+  suma = primernumero + segundonumero;
+
+  alert("La suma es: "+suma);
 }
 
